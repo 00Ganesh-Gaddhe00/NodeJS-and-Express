68fbf3597d01b85953234dff25a0eac677c77654
Ganesh <00mr.lg00@gmail.com>
2024-04-11 17:46:20 +0530

    server creation and fucntion without MVC

---
+++ class4/server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const fs = require("fs")
+
+
+const port = 8082
+const app = express();
+
+const data = JSON.parse(fs.readFileSync("../class3/data.json", 'utf-8'))
+// console.log(data)
+const products = data.products
+
+
+console.log(products);
+app.get('/', (req,res)=>{
+    res.send("dummy jason data")
+})
+
+app.get('/products', (req, res)=>{
+    res.send(products)
+})
+
+app.get('/products/:id', (req, res)=>{
+    //console.log(req.params)//req.params is an object
+     const id = req.params.id
+    const product = products.find((ele)=>ele.id==id)
+
+    if(!product)res.send("No product present with the given id")
+    else res.send(product);
+})
+
+app.use(express.json())//parse incoming JSON requests
+
+app.post('/products', (req, res)=>{
+    //console.log(req.body)
+    products.push(req.body)
+    res.send("post")
+})
+
+app.delete('/products/:id', (req, res)=>{
+    const id = req.params.id
+    const productIndex = products.findIndex((ele)=> ele.id==id)
+    products.splice(productIndex,1)
+    res.send("product deleted")
+})
+
+app.patch('/products/:id', (req, res)=>{
+    const id = req.params.id
+    const productIndex = products.findIndex((ele)=>ele.id==id);
+    const product = products[productIndex];
+    products.splice(productIndex,1,{...product,...req.body})
+    res.send("updating the existing key")
+})
+
+app.put('/products/:id', (req, res)=>{
+    const id =req.params.id
+    const productIndex = products.findIndex((ele)=>ele.id==id);
+    products[productIndex] = req.body
+    res.send("updating the exsiting key")
+
+})
+
+app.listen(port,()=>{
+    console.log(`server is listing at ${port}`)
+})
+
